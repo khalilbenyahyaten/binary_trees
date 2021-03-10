ca3958c753f42489d20c7be265f1fdc4e801a06a
khalilbenyahyaten <2300@holbertonschool.com>
2021-03-10 17:12:51 +0000

    insert right child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inser node at left child
+ * binary_tree_insert_left - insert node at right child
  *
  *@parent: parent
  *@value: value
@@ -8,7 +8,7 @@
  *Return: new node or null
  *
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
 if (parent == NULL)
